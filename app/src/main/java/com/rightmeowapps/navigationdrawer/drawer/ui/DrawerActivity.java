fa6805a8ec512ef17c22969a5190b27aab3f2975
akiniyalocts <kiniyaloca1@gmail.com>
2015-11-02 10:22:50 -0500

    fix package name for material icon textview

--- app/src/main/java/com/rightmeowapps/navigationdrawer/drawer/ui/DrawerActivity.java
+++ app/src/main/java/com/rightmeowapps/navigationdrawer/drawer/ui/DrawerActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Gravity;
 import android.view.MenuItem;
 
 import com.rightmeowapps.navigationdrawer.R;
